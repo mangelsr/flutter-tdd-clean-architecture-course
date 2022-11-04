1cd01f1655602e681a1b2497f0ae0e0ab48971fd
Miguel Sanchez <mangelsr25@gmail.com>
2022-11-04 11:01:08 -0500

    [ADD]: Test for NetworkInfo plugin wrapper

---
+++ test/core/network/network_info_test.dart
@@ -0,0 +1,35 @@
+import 'package:cross_connectivity/cross_connectivity.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:number_trivia/core/network/network_info.dart';
+
+@GenerateNiceMocks([MockSpec<Connectivity>()])
+import 'network_info_test.mocks.dart';
+
+void main() {
+  late MockConnectivity mockConnectivity;
+  late NetworkInfoImpl networkInfoImpl;
+
+  setUp(() {
+    mockConnectivity = MockConnectivity();
+    networkInfoImpl = NetworkInfoImpl(mockConnectivity);
+  });
+
+  group('isConnected', () {
+    test('should forward the call to cross_connectivity package', () async {
+      // arrange
+      final testConnectionFuture = Future.value(true);
+      when(mockConnectivity.checkConnection())
+          .thenAnswer((_) => testConnectionFuture);
+
+      // act
+      final result = networkInfoImpl.isConnected;
+
+      // assert
+      verify(mockConnectivity.checkConnection());
+      expect(result, testConnectionFuture);
+    });
+  });
+}
