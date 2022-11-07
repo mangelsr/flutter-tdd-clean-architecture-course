c7c72bf74c95210a72b88bb4f7be83b02fb1ef1d
Miguel Sanchez <mangelsr25@gmail.com>
2022-11-07 12:05:14 -0500

    [ADD]: Initial bloc config with events

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  NumberTriviaBloc() : super(Empty()) {
+    on<NumberTriviaEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
