31714c0497c63fc7a2dbcb6e830d5c64ae10a8df
Miguel Sanchez <mangelsr25@gmail.com>
2022-11-08 16:11:18 -0500

    [UPD]: main function call to use injection_container

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'injection_container.dart' as di;
 
-void main() => runApp(const MyApp());
+main() async {
+  await di.init();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
